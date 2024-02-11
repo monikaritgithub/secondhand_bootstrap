84aa881eb18134fc2f1fd74612bc35f337f301f2
Smriti Bhandari <moonekabhandari123@gmail.com>
2024-02-11 06:49:43 -0900

    bootstrap_secondhandproduct

---
+++ script.js
@@ -0,0 +1,6 @@
+  document.getElementById('contactForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    // Add your form submission logic here
+    // For example, use AJAX to send form data to the server
+    // and display a success message to the user
+  });
